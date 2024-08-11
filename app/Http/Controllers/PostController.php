d816be2e6bd0a6271d1959d93d6e8097a126ae85
Anton Vix <Gordan.ei@protonmail.ch>
2024-08-12 03:48:15 +0400

    PostService created

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,36 +6,24 @@ use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
-use Illuminate\Database\Eloquent\Builder;
+use App\Services\PostService;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function __construct(
+        private readonly PostService $service
+    ) {
+        //
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index(Request $request)
     {
-        $sortColumn = $request->input('sort', 'id');
-        $sortDirection = $request->input('order') === 'desc' ? 'desc' : 'asc';
-
-        $query = Post::query()
-            ->orderBy($sortColumn, $sortDirection)
-            ->when($request->filled('title'), function (Builder $query) use ($request) {
-                $query->where('title', 'like', '%'.$request->input('title').'%');
-            })
-            ->when($request->filled('body'), function (Builder $query) use ($request) {
-                $query->where('body', 'like', '%'.$request->input('body').'%');
-            })
-            ->when($request->filled('author') && is_numeric($request->input('author')), function (Builder $query) use ($request) {
-                $query->where('author_id', $request->input('author'));
-            })
-            ->when($request->filled('created_at'), function (Builder $query) use ($request) {
-                $query->whereDate('created_at', $request->input('created_at'));
-            });
-
         return PostResource::collection(
-            $query->paginate(10)
+            $this->service->index($request)
         );
     }
 
---
+++ app/Services/PostService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+class PostService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index(Request $request)
+    {
+        $sortColumn = $request->input('sort', 'id');
+        $sortDirection = $request->input('order') === 'desc' ? 'desc' : 'asc';
+
+        return Post::query()
+            ->orderBy($sortColumn, $sortDirection)
+            ->when($request->filled('title'), function (Builder $query) use ($request) {
+                $query->where('title', 'like', '%'.$request->input('title').'%');
+            })
+            ->when($request->filled('body'), function (Builder $query) use ($request) {
+                $query->where('body', 'like', '%'.$request->input('body').'%');
+            })
+            ->when($request->filled('author') && is_numeric($request->input('author')), function (Builder $query) use ($request) {
+                $query->where('author_id', $request->input('author'));
+            })
+            ->when($request->filled('created_at'), function (Builder $query) use ($request) {
+                $query->whereDate('created_at', $request->input('created_at'));
+            })
+            ->paginate(10);
+    }
+}
