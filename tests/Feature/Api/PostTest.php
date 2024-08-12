539cf2538108689cb4475f69da2e55185bf9b700
Anton Vix <Gordan.ei@protonmail.ch>
2024-08-12 21:10:45 +0400

    events test created

--- tests/Feature/Api/PostTest.php
+++ tests/Feature/Api/PostTest.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Api;
 
+use App\Events\PostCreated as PostCreatedEvent;
+use App\Listeners\SendPostCreatedNotification;
 use App\Mail\PostCreated;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
@@ -60,8 +63,6 @@ class PostTest extends TestCase
 
     public function test_api_posts_store_request_is_successful(): void
     {
-        Mail::fake();
-
         $data = [
             'title' => $this->faker->sentence,
             'body' => $this->faker->paragraph,
@@ -73,10 +74,33 @@ class PostTest extends TestCase
             ->assertJsonStructure([
                 'data' => $this->postCollectionStructure,
             ]);
+    }
+
+    public function test_after_api_posts_store_request_email_is_sent(): void
+    {
+        Mail::fake();
+
+        $data = [
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->paragraph,
+            'author' => $this->user->id,
+        ];
+
+        $this->postJson(route('posts.store'), $data);
 
         Mail::assertQueued(PostCreated::class, function ($mail) {
             return $mail->hasTo($this->user->email);
         });
+    }
+
+    public function test_post_created_event_is_being_listened(): void
+    {
+        Event::fake();
+
+        Event::assertListening(
+            PostCreatedEvent::class,
+            SendPostCreatedNotification::class
+        );
     }
 
     public function test_api_posts_show_request_is_successful(): void
