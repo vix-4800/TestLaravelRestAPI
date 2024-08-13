eca281001a10b902cf2e74a429d0c1d04e2e2c96
Anton Vix <Gordan.ei@protonmail.ch>
2024-08-13 04:59:33 +0400

    ide helper update

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -11,3 +11,56 @@
  */
 
 
+namespace App\Models{
+/**
+ * 
+ *
+ * @property-read \App\Models\User|null $author
+ * @method static \Database\Factories\PostFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Post newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post query()
+ */
+	class Post extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property mixed $password
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\UserActivity> $activities
+ * @property-read int|null $activities_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Post> $posts
+ * @property-read int|null $posts_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ */
+	class User extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property-write mixed $method
+ * @property-read \App\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity failed()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity forLastDays(int $days)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity forLastMonth()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity fromDate(\Illuminate\Support\Carbon $from)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity query()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity successful()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserActivity toDate(\Illuminate\Support\Carbon $to)
+ */
+	class UserActivity extends \Eloquent {}
+}
+
